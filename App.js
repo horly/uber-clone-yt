3a97a0a0689a9be2ca0f385b110be760144c1676
horly <andmatboy@gmail.com>
2023-01-01 16:10:41 +0100

    home screen created

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+/**
+ * 1) installation de Redux
+ * # NPM
+ * - npm install @reduxjs/toolkit
+ * - npm install react-redux
+ * # YARN
+ * - yarn add @reduxjs/toolkit
+ * Docs : https://redux.js.org/introduction/getting-started
+ */
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is my app</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+        <View style={styles.container}>
+          <Text>This is my app</Text>
+        </View>
+    </Provider>
   );
 }
 
