86cac15538d4ed083d624f355a234e5d86ae8db4
horly <andmatboy@gmail.com>
2023-01-01 18:23:42 +0100

    Navigation added

--- App.js
+++ App.js
@@ -2,7 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
 import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
 
 /**
  * 1) installation de Redux
@@ -15,9 +21,29 @@ import { store } from './store';
  */
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
-        <HomeScreen/>
+        <NavigationContainer>
+          <SafeAreaProvider>
+            <Stack.Navigator>
+              <Stack.Screen 
+                name="HomeScreen" 
+                component={HomeScreen} 
+                options={{
+                  headerShown: false
+                }}
+              />
+              <Stack.Screen 
+                name="MapScreen" 
+                component={MapScreen} 
+                options={{
+                  headerShown: false
+                }}
+              />
+            </Stack.Navigator>
+          </SafeAreaProvider>
+        </NavigationContainer>
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,29 +1,64 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { FlatList, Text, StyleSheet, TouchableOpacity, View, Image } from 'react-native'
+import tw from 'twrnc';
+import { Icon } from '@rneui/themed'; //react native element design
+import { useNavigation } from '@react-navigation/native';
 
 const data = [
     {
         id: '123',
         title: "Get a ride",
-        image:  "../assets/img/car.png",
+        image:  require('../assets/img/car.png'),
         screen: "MapScreen",
     },
     {
         id: '456',
         title: "Order food",
-        image:  "../assets/img/food.png",
+        image:  require('../assets/img/food.png'),
         screen: "EatsScreen",
     }
 ];
 
 const NavOptions = () => {
+    const navigation = useNavigation();
+
     return (
         <View>
-            <Text>Hello word</Text>
+            <FlatList
+                data={data}
+                keyExtractor={ (item) => item.id }
+                horizontal
+                renderItem={ ({item}) => (
+                    <TouchableOpacity 
+                        onPress={() => navigation.navigate(item.screen)}
+                        style={ tw`p-2-pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40` }
+                    >
+                        <View>
+                            <Image
+                                style={ styles.optionImg }
+                                source={ item.image }
+                            />
+                            <Text style={tw`mt-2 text-lg font-semibold`}>{ item.title }</Text>
+                            <Icon 
+                                style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                                type='antdesign' name='arrowright' color="white" 
+                            />
+                        </View>
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     )
 }
 
 export default NavOptions
+
+const styles = StyleSheet.create({
+    optionImg:{
+        width: 120, 
+        height: 120, 
+        resizeMode: 'contain'
+    }
+});
 
 
