05769b6c510454526626a163d46454f910fdfd00
horly <andmatboy@gmail.com>
2023-01-01 16:40:50 +0100

    Uber logo added

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 
 /**
@@ -16,9 +17,7 @@ import { store } from './store';
 export default function App() {
   return (
     <Provider store={store}>
-        <View style={styles.container}>
-          <Text>This is my app</Text>
-        </View>
+        <HomeScreen/>
     </Provider>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,33 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
+import tw from 'twrnc';
+
+/**
+ * Tailwind React Native Classnames 🏄‍♂️
+ * installation 
+ * - npm install twrnc
+ */
 
 const HomeScreen = () => {
     return (
-        <View>
-            <Text>This is Home screen</Text>
-        </View>
+        <SafeAreaView style={tw`bg-white h-full`}>
+            <View style={tw`p-5`}>
+                <Image
+                    style={styles.logoImg}
+                    source={require('../assets/img/Uber_logo_2018.png')}
+                    //source={{ uri: "https://links.parareact.com/gzs", }}
+                />
+            </View>
+        </SafeAreaView>
     )
 };
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-
+    logoImg:{
+        width: 100, 
+        height: 100, 
+        resizeMode: 'contain'
+    }
 });
